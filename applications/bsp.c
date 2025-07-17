c7918b74fb1cd4ab63b4e3fd70f01428a31ccfb4
T0n0T <823478402@qq.com>
2025-07-17 09:46:57 +0000

    add systemview for trace

--- applications/bsp.c
+++ applications/bsp.c
@@ -7,6 +7,7 @@
 #include "log.h"
 #ifdef DEBUG
 #include "cm_backtrace.h"
+#include "SEGGER_SYSVIEW.h"
 #endif
 
 /* Use for sleep judgement */
@@ -48,7 +49,9 @@ void RTC_WKUP_IRQHandler(void)
 
 void SysTick_Handler(void)
 {
+    SEGGER_SYSVIEW_RecordEnterISR();
     QTIMEEVT_TICK_X(0, 0);
+    SEGGER_SYSVIEW_RecordExitISR();
     QV_ARM_ERRATUM_838869();
 }
 
@@ -80,29 +83,30 @@ void BSP_init(void)
      */
 #ifdef DEBUG
     cm_backtrace_init("build/n32l43x", "V1.0", "1.0.0");
-    GPIO_ConfigPinRemap(GPIOB_PORT_SOURCE, GPIO_PIN_SOURCE3, GPIO_NO_AF);
-    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-    TPI->ACPR = 7;
-    TPI->SPPR = 2;
-    TPI->FFCR = 0x100;
-    DWT->CTRL = (1 << DWT_CTRL_CYCTAP_Pos)       // Prescaler for PC sampling
-                                                 // 0 = x64, 1 = x1024
-                | (0 << DWT_CTRL_POSTPRESET_Pos) // Postscaler for PC sampling
-                                                 // Divider = value + 1
-                | (1 << DWT_CTRL_PCSAMPLENA_Pos) // Enable PC sampling
-                | (2 << DWT_CTRL_SYNCTAP_Pos)    // Sync packet interval
-                                                 // 0 = Off, 1 = Every 2^23 cycles,
-                                                 // 2 = Every 2^25, 3 = Every 2^27
-                | (1 << DWT_CTRL_EXCTRCENA_Pos)  // Enable exception trace
-                | (1 << DWT_CTRL_CYCCNTENA_Pos); // Enable cycle counter
+    SEGGER_SYSVIEW_Conf();
+    // GPIO_ConfigPinRemap(GPIOB_PORT_SOURCE, GPIO_PIN_SOURCE3, GPIO_NO_AF);
+    // CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    // TPI->ACPR = 7;
+    // TPI->SPPR = 2;
+    // TPI->FFCR = 0x100;
+    // DWT->CTRL = (1 << DWT_CTRL_CYCTAP_Pos)       // Prescaler for PC sampling
+    //                                              // 0 = x64, 1 = x1024
+    //             | (0 << DWT_CTRL_POSTPRESET_Pos) // Postscaler for PC sampling
+    //                                              // Divider = value + 1
+    //             | (1 << DWT_CTRL_PCSAMPLENA_Pos) // Enable PC sampling
+    //             | (2 << DWT_CTRL_SYNCTAP_Pos)    // Sync packet interval
+    //                                              // 0 = Off, 1 = Every 2^23 cycles,
+    //                                              // 2 = Every 2^25, 3 = Every 2^27
+    //             | (1 << DWT_CTRL_EXCTRCENA_Pos)  // Enable exception trace
+    //             | (1 << DWT_CTRL_CYCCNTENA_Pos); // Enable cycle counter
 
-    /* Configure instrumentation trace macroblock */
-    ITM->LAR = 0xC5ACCE55;
-    ITM->TCR = (1 << ITM_TCR_TraceBusID_Pos) // Trace bus ID for TPIU
-               | (1 << ITM_TCR_DWTENA_Pos)   // Enable events from DWT
-               | (1 << ITM_TCR_SYNCENA_Pos)  // Enable sync packets
-               | (1 << ITM_TCR_ITMENA_Pos);  // Main enable for ITM
-    ITM->TER = 0xFFFFFFFF;                   // Enable all stimulus ports
+    // /* Configure instrumentation trace macroblock */
+    // ITM->LAR = 0xC5ACCE55;
+    // ITM->TCR = (1 << ITM_TCR_TraceBusID_Pos) // Trace bus ID for TPIU
+    //            | (1 << ITM_TCR_DWTENA_Pos)   // Enable events from DWT
+    //            | (1 << ITM_TCR_SYNCENA_Pos)  // Enable sync packets
+    //            | (1 << ITM_TCR_ITMENA_Pos);  // Main enable for ITM
+    // ITM->TER = 0xFFFFFFFF;                   // Enable all stimulus ports
 #endif
     NVIC_SetPriorityGrouping(4);
     NVIC_SetPriority(SysTick_IRQn, DEF_ISR_PRI);
@@ -168,6 +172,8 @@ void QF_onStartup(void)
 {
     RCC_ClocksType RCC_ClockFreq;
     RCC_GetClocksFreqValue(&RCC_ClockFreq);
+    extern uint32_t SystemCoreClock;
+    SystemCoreClock = RCC_ClockFreq.SysclkFreq;
     APP_LOG_INFO("SYSCLK: %u", (unsigned int)RCC_ClockFreq.SysclkFreq);
     APP_LOG_INFO("HCLK: %u", (unsigned int)RCC_ClockFreq.HclkFreq);
     APP_LOG_INFO("PCLK1: %u", (unsigned int)RCC_ClockFreq.Pclk1Freq);
